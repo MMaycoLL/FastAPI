be8afcfface985a37e76fc36d54cabaf7781fefa
mmaycol <wolf.malign@gmail.com>
2023-01-24 23:21:28 -0500

    Documentación automática con Swagger

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
+# cambiar el nombre de la aplicación
+app.title = "Mi aplicación con FastAPI"
+
+# cambiar la version de la aplicación
+app.version = "0.0.1"
+
+# los tags nos permite agrupar las rutas de la aplicación
+
+@app.get('/', tags=['home'])
 def message():
     return "Hello world!!!"
