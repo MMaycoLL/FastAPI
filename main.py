d79bfeace2e954c6820b3d0f044cae202232e901
mmaycol <wolf.malign@gmail.com>
2023-01-31 21:48:11 -0500

    validacion de tipos de datos

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, Field
 from typing import Optional
 
 app = FastAPI()
@@ -13,11 +13,23 @@ app.version = "0.0.1"
 
 class Movie(BaseModel):
     id: Optional[int] = None
-    title: str
-    overview: str
-    year: int
-    rating: float
-    category: str
+    title: str = Field(min_length=5, max_length=15)
+    overview: str = Field(min_length=15, max_length=50)
+    year: int = Field(le=2022)
+    rating: float = Field(le=10)
+    category: str = Field(min_length= 5, max_length=12) 
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "id": 1,
+                "title": "Mi pelicula",
+                "overview": "Descripcion de la pelicula",
+                "year": 2022,
+                "rating": 9.8,
+                "category": "accion"
+            }
+        }
 
 movies = [
     {
